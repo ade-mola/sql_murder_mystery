973691e86258d4c893b4423ac1e09d1ac1dd68ea
Ademola Kunmi <salamiolokun@gmail.com>
2022-03-13 03:20:17 +0000

    solve murder using SQL

--- find_the_benefactor.sql
+++ find_the_benefactor.sql
@@ -1,25 +1,32 @@
 -- We found the murderer but he wasn't the mastermind. There is still a guilty party on the loose.
 -- We go back into the 'interview' table to get the murderer's statement
 
-SELECT * FROM interview
+SELECT *
+FROM interview
 WHERE person_id = 67318;
 
 -- The murderer's statement proved useful. He gave details of the person who hired him.
 -- He was hired by a woman with height between 65" and 67", has red hair, drives a Tesla Model S.
 -- She also attend the SQL Symphony Concert thrice in December 2017
 
-SELECT p.id, p.name,
-	   dl.height, dl.hair_color,
-	   dl.car_model, dl.car_make,
-	   f.event_name, f.date
+SELECT p.id,
+       p.name,
+       dl.height,
+       dl.hair_color,
+       dl.car_model,
+       dl.car_make,
+       f.event_name,
+       f.date
 FROM person p
-JOIN drivers_license dl
-    ON p.license_id = dl.id
-JOIN facebook_event_checkin f
-    ON p.id = f.person_id
-WHERE (dl.height BETWEEN 65 AND 67) AND dl.hair_color = 'red'
-      AND (dl.car_make = 'Tesla' AND dl.car_model = 'Model S')
-	  AND f.event_name = 'SQL Symphony Concert' AND f.date LIKE '%201712%'
+         JOIN drivers_license dl
+              ON p.license_id = dl.id
+         JOIN facebook_event_checkin f
+              ON p.id = f.person_id
+WHERE (dl.height BETWEEN 65 AND 67)
+  AND dl.hair_color = 'red'
+  AND (dl.car_make = 'Tesla' AND dl.car_model = 'Model S')
+  AND f.event_name = 'SQL Symphony Concert'
+  AND f.date LIKE '%201712%';
 
 
 -- We found our second culprit! MIRANDA PRIESTLY was the brains behind the murder.
--- find_the_murderer.sql
+++ find_the_murderer.sql
@@ -1,41 +1,48 @@
 -- We start by checking the 'crime_scene_report' table to get details of the murder narrowing down to 'SQL City'
 -- and on 2018-01-15
 
-SELECT * FROM crime_scene_report
-WHERE city = 'SQL City' AND date = '20180115'
+SELECT *
+FROM crime_scene_report
+WHERE city = 'SQL City'
+  AND date = '20180115';
 
 -- Result from query showed there were two witnesses. One witness lives in the last house on 'Northwestern Dr'
 -- Second witness is named 'Annabel' and lives on 'Franklin Ave'
 -- Next, we get the information of the witnesses from the 'person' table
 
-SELECT * FROM person
-WHERE (name LIKE '%Annabel%' and address_street_name LIKE 'Franklin Ave') 
-		   OR address_street_name LIKE 'Northwestern Dr'
-ORDER BY address_street_name ASC, address_number DESC
+SELECT *
+FROM person
+WHERE (name LIKE '%Annabel%' and address_street_name LIKE 'Franklin Ave')
+   OR address_street_name LIKE 'Northwestern Dr'
+ORDER BY address_street_name, address_number DESC;
 
 -- Having gotten the identities of our witnesses, we search the 'interview' table to get their statements with their IDs
 
-SELECT * FROM interview
-WHERE person_id = 16371 OR person_id = 14887
+SELECT *
+FROM interview
+WHERE person_id = 16371
+   OR person_id = 14887
 
 -- From the witnesses statements, we know the culprit is a GOLD member of the gym,
 -- has a membership id that includes '48Z', plate number also include 'H42W',
 -- and went to the gym on '2018-01-09' 
 
-WITH cte_person AS 
+    WITH cte_person AS
     (
         SELECT * FROM person p
         JOIN drivers_license dl
         ON p.license_id = dl.id
     )
- 
-SELECT cp.* FROM get_fit_now_member m
-JOIN cte_person cp
-    ON m.person_id = cp.id
-JOIN get_fit_now_check_in ci
-    ON m.id = ci.membership_id
-WHERE membership_status = 'gold' and check_in_date = '20180109' 
-	  and membership_id like '%48Z%' and plate_number like '%H42W%'
 
+SELECT cp.*
+FROM get_fit_now_member m
+         JOIN cte_person cp
+              ON m.person_id = cp.id
+         JOIN get_fit_now_check_in ci
+              ON m.id = ci.membership_id
+WHERE m.membership_status = 'gold'
+  and ci.check_in_date = '20180109'
+  and m.id like '%48Z%'
+  and cp.plate_number like '%H42W%';
 
 -- Finally! The murderer has been unmasked and his name is JEREMY BOWERS!
